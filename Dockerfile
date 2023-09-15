685e3599e4e9a70d64c321369dd980892accf280
mirmannan99 <mirmannan99@gmail.com>
2023-09-15 15:56:57 +0530

    removed db

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+#build stage
 From node:18-alpine
 
 WORKDIR /usr/src/app
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
-import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
+  imports: [TasksModule],
 })
 export class AppModule {}
